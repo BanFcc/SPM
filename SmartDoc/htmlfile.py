a875d91d47771e4edfc57b9c2eb6f3695bf7591c
BanFcc <39665166+BanFcc@users.noreply.github.com>
2019-04-08 21:13:49 +0800

    Update htmlfile.py

--- SmartDoc/htmlfile.py
+++ SmartDoc/htmlfile.py
@@ -1,7 +1,40 @@
 
 # -*- coding: utf-8 -*-
 #help create a html file
-class HtmlFile:            
-    def __init__(self,title):
-        self.title=title
-    def addText(self,text):
+class HtmlFile:
+    def changeString(self,str):
+        newStr=""
+        for c in str:
+            if c == ' ':
+                newStr += "&nbsp;"
+            elif c=='"':
+                newStr += "&quot;"
+            elif c == "&":
+                newStr += "&amp;"
+            elif c=='<':
+                newStr += "&lt;"
+            elif c == '>':
+                newStr += "&gt;"
+            else:
+                newStr+=c
+        str=newStr
+    class Node:
+        def __init__(self, info, idx, flag):  
+            self.idx=idx
+            self.son = []
+            self.info = info
+            self.flag = flag
+    def __init__(self, title):
+        self.title = title
+        self.webSite = []
+        self.cnt = 0
+    def addNode(self, info, flag):
+        self.webSite.append(self.Node(info, self.cnt + 1, 1))
+        self.cnt+=1
+    def addText(self, text):
+        self.changeString(text)
+        self.addNode(text,1)
+
+
+
+        
