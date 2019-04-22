f5e7f3cf382e6af7767ccd6221610945ddee19e1
BanFcc <39665166+BanFcc@users.noreply.github.com>
2019-04-22 20:39:31 +0800

    Update htmlfile.py

--- SmartDoc/htmlfile.py
+++ SmartDoc/htmlfile.py
@@ -1,8 +1,11 @@
 
 # -*- coding: utf-8 -*-
 #help create a html file
+textFlag = 1
+urlFlag = 2
+
 class HtmlFile:
-    def changeString(self,str):
+    def ChangeString(self,str):
         newStr = ""
         for c in str:
             if c == ' ':
@@ -20,66 +23,81 @@ class HtmlFile:
         str = newStr
         
     class Node:
-        def __init__(self, info, idx, flag):  #flag=1 文本  flag=2超链接 
+        def __init__(self, info, idx, flag, Id=""):  #flag=1 文本  flag=2超链接 
             self.idx=idx
             self.son = []
             self.info = info
             self.flag = flag
+            self.Id=Id
 
     def __init__(self, title):
         self.title = title
         self.webSite = []
         self.cnt = 0
 
-    def addNode(self, info, flag):
-        self.webSite.append(self.Node(info, self.cnt + 1, 1))
+    def AddNode(self, info, flag,Id=""):
+        self.webSite.append(self.Node(info, self.cnt + 1, flag, Id))
         self.cnt += 1
 
-    def addText(self, text):
-        self.changeString(text)
-        self.addNode(text, 1)
+    def AddText(self, text):
+        self.ChangeString(text)
+        self.AddNode(text, 1)
 
-    def addUrl(self, text, url):
-        self.changeString(text)
-        self.addNode((text, url), 2)
+    def AddUrl(self, text, url, Id=""):
+        self.ChangeString(text)
+        self.AddNode((text, url), 2,Id)
 
         
-    def writeHead(self, title, css):
+    def WriteHead(self, title, css):
         res = []
-        res.append("<head>")
-        res.append("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />")
-        res.append("<title>" + title + "</title>")
-        res.append("<link rel=\"stylesheet\" type=\"text/css\" href=\"materials/" + css + "\" />")
+        res.append(r'<head>')
+        res.append(r'<meta charset="utf-8">')
+        res.append(r'<title>' + title + r'</title>')
+        res.append(r'</head>')
+        #no css
         return res
 
-    def writeText(self, textNode):
-        res = ""
-        res += "<div id=\"left\">\n"
-        res += "  <p>" + textNode.info + "</p>\n"
-        res += "</div>"
+    def WriteText(self, textNode):
+        res = []
+        res.append(r'<div id="left">')
+        res.append(r'<p>' + textNode.info + r'</p>')
+        res.append(r'</div>')
         return res
 
-    def writeUrl(self, urlNode):
-        #<a href="J.Super Brain.html">Super Brain</a>
-        res = ""
-        res += "<a href=\"" + urlNode.info(1) + "\">" + urlNode.info(0) + "</a>"
+    def WriteUrl(self, urlNode):
+        #info(1)url   info(0)text
+        res = []
+        s = r'<a '
+        if urlNode.Id!="":
+            s += r'id="' + urlNode.Id + r'" '
+        s +=r'href=' +urlNode.info[1] + r'">' + urlNode.info[0] + r'</a>'
+        res.append(s)
         return res
-
-    def dfs(self, node, num):
-        for v in node.son:
-            self.dfs(v, num + 2)
-        
-         
-    def creatFile(self, fileName):
-        fp = open(fileName + ".html", w)
-        fp.write("<html xmlns=\" http: // www.w3.org / 1999 / xhtml \">" + "\n")
-        fp.writelines(self.writeHead(self.title, "myCss.css"))
-        for node in self.webSite:
-            fp.writelines(self.dfs(node, 0))
     
+    def Dfs(self, node, num):
+        for v in node.son:
+            self.Dfs(v, num + 2)
+    def WriteNode(self, node):
+        if node.flag == 1:
+            return self.WriteText(node)
+        elif node.flag==2:
+            return self.WriteUrl(node)
+    def WirtLinesln(self,fp,lines):
+        for s in lines:
+            fp.write(s+'\n')
+    def CreatFile(self, fileName):
+        fp = open(fileName + r'.html','w')
+        fp.write(r'<html>' + "\n")
+        
+        self.WirtLinesln(fp,self.WriteHead(self.title,"myCss.css"))
+        fp.write(r'<body>'+'\n')
+        for node in self.webSite:
+            self.WirtLinesln(fp,self.WriteNode(node))
+        fp.write(r'</body>'+'\n')
+        fp.write(r'</html>'+'\n')
         fp.close()
-        
-        
+if if __name__ == "__main__":
+    pass
 
 
         
